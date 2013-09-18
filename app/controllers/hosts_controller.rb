02933d77ff47b36bdcd04c08a34820fa33b93294
Sam Shaaban <sam@nurelm.com>
2013-09-17 22:58:10 -0400

    Add show method to host controller / view.

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,2 +1,13 @@
 class HostsController < ApplicationController
+  def index
+    host = Host.find(:all, order: 'name').first
+    if host != nil then
+      redirect_to host_url(host)
+    end
+  end
+
+  def show
+    @host = Host.find(params[:id])
+    @hosts = Host.find(:all, order: 'name')
+  end
 end
