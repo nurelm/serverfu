94651fb8572b9e13a9fefecc10917d81017d0b01
Sam Shaaban <sam@nurelm.com>
2013-09-20 11:36:53 -0400

    Add special case for root screen.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
 
   def nav_link(link_text, link_path)
-    class_value = (request.original_fullpath.include? link_path) ? 'active' : ''
+    if link_path == "/" then
+      class_value = current_page?(link_path) ? 'active' : ''
+    else
+      class_value = (request.original_fullpath.include? link_path) ? 'active' : ''
+    end
+
     content_tag(:li, :class => class_value) do
       link_to link_text, link_path
     end
