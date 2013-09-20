0f3174e3cca5d6bd6b07752570548c1c1c20ab28
Sam Shaaban <sam@nurelm.com>
2013-09-20 11:32:29 -0400

    Update helper to highlight correct links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
 
   def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
-    content_tag(:li, :class => class_name) do
+    class_value = (request.original_fullpath.include? link_path) ? 'active' : ''
+    content_tag(:li, :class => class_value) do
       link_to link_text, link_path
     end
   end
 
 end
-
