21f58b4bbb4e877a916fa917f62e45360d5afe69
Sam Shaaban <sam@nurelm.com>
2013-09-20 22:45:43 -0400

    Add in-place editing.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,2 +1,24 @@
 class ServersController < ApplicationController
+
+  def update
+     @server = Server.find params[:id]
+   
+     respond_to do |format|
+       if @server.update_attributes(server_params)
+         format.html { redirect_to(@server, :notice => 'Server was successfully updated.') }
+         format.json { respond_with_bip(@server) }
+       else
+         format.html { render :action => "edit" }
+         format.json { respond_with_bip(@server) }
+       end
+     end
+   end
+
+
+  private
+
+  def server_params
+    params.require(:server).permit(:name)
+  end
+
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -2,4 +2,17 @@ class Server < ActiveRecord::Base
   has_many :notes, :as => :noteable
   has_many :clients
   belongs_to :host
+
+  def num_clients
+    clients.length
+  end
+
+  def num_sites
+    num_sites = 0
+    clients.each do |client|
+      num_sites += client.sites.length
+    end
+    num_sites
+  end
+
 end
